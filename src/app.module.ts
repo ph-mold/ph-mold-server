aa9612768c2301a350d49207663161527eeee538
l11040 <l1104033837@gmail.com>
2025-04-02 16:01:18 +0900

    [Add] DB 헬스체크 라우터 구성 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './db/db.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DbModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DbModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })

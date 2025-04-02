8b1d7b18ed74b8965940e5068415f1e1c591c0d7
l11040 <l1104033837@gmail.com>
2025-04-02 16:00:40 +0900

    [Add] DB Connection Config추가

---
+++ src/db/db.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host:
+          process.env.NODE_ENV === 'production'
+            ? configService.get('DOCKER_DB_HOST')
+            : configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+        timezone: 'Z',
+      }),
+    }),
+  ],
+  exports: [TypeOrmModule],
+})
+export class DbModule {}

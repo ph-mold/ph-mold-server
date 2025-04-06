504ecb07c267b0d9c077fcd62f9c8f48db791fb0
l11040 <l1104033837@gmail.com>
2025-04-06 19:34:36 +0900

    [Chore] 태그 제품 조회 태스트용 엔드포인트

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ProductService } from './product.service';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+  @Get('/test')
+  async test() {
+    const tagKeys = ['pp', 'syringe'];
+    return this.productService.getProductsByTagKeys(tagKeys);
+  }
+}

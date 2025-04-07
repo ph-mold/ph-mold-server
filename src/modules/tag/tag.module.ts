b36605d700f02bc3e1344a782838a188c4f3a8cf
l11040 <l1104033837@gmail.com>
2025-04-07 20:45:09 +0900

    [Add] 태그키로 태그 조회 추가

--- src/modules/tag/tag.module.ts
+++ src/modules/tag/tag.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tag } from './entities/tag.entity';
+import { TagRepository } from './tag.repositroy';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Tag])],
+  providers: [TagRepository],
+  exports: [TagRepository],
 })
 export class TagModule {}

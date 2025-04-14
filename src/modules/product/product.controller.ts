6c61d7be371ddc0bd5284b5c3dff3bbfc3b708bb
l11040 <l1104033837@gmail.com>
2025-04-14 21:31:25 +0900

    [Add] 카테고리로 제품 리스트 조회 기능 추가

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,15 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ProductService } from './product.service';
 
-@Controller('product')
+@Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
-  @Get('/test')
-  async test() {
-    const tagKeys = ['syringe'];
-    return this.productService.getProductsByTagKeys({
-      include: tagKeys,
-      exclude: ['1ml', 'clean-syringe'],
-    });
+
+  @Get()
+  async getProductsByCategoryKey(@Query('category') categoryKey: string) {
+    return this.productService.getProductsByCategoryKey(categoryKey);
   }
 }

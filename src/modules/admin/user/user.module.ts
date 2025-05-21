264723d6503b7ca4203caa9f5baa8010d5608c3f
l11040 <l1104033837@gmail.com>
2025-05-21 17:31:17 +0900

    [Add] 유저 조회 권한 설정

--- src/modules/admin/user/user.module.ts
+++ src/modules/admin/user/user.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 import { UserRepository } from './user.repository';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UserService, UserRepository],
   exports: [UserService],
+  controllers: [UserController],
 })
 export class UserModule {}

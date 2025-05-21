d2779fdf40e799cf9abdf6a733768a5bc8b9f830
l11040 <l1104033837@gmail.com>
2025-05-21 17:32:06 +0900

    [Add] 제품 상세정보 변경 role추가

--- src/modules/admin/product/admin.product.controller.ts
+++ src/modules/admin/product/admin.product.controller.ts
@@ -3,17 +3,25 @@ import {
   Controller,
   Param,
   Patch,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { AdminProductService } from './admin.product.service';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Role, Roles } from 'src/decorators/roles.decorator';
+import { RolesGuard } from 'src/guards/roles.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
 
 @Controller('admin/products')
 export class AdminProductController {
   constructor(private readonly adminProductService: AdminProductService) {}
 
   @Patch(':id')
+  @ApiBearerAuth('access-token')
+  @Roles(Role.Admin, Role.User)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @UsePipes(new ValidationPipe({ transform: true }))
   async updateProduct(@Param('id') id: number, @Body() dto: UpdateProductDto) {
     return await this.adminProductService.update(id, dto);

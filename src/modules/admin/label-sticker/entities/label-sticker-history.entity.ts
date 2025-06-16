5fb177c2c33291019a51a2d80e066795a32770ec
l11040 <l1104033837@gmail.com>
2025-06-16 15:05:11 +0900

    [Add] 라벨 스티커 히스토리 엔티티 추가

---
+++ src/modules/admin/label-sticker/entities/label-sticker-history.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('label_sticker_histories')
+export class LabelStickerHistory {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  file_name: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @Column({ type: 'varchar', length: 100 })
+  operator: string;
+
+  @Column({ name: 'label_type', type: 'varchar', length: 50 })
+  labelType: string;
+
+  @Column({ name: 'label_data', type: 'jsonb' })
+  labelData: Record<string, any>;
+}

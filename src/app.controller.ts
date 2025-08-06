9d468f2b67135882a6221fadbf3087931e236df0
l11040 <l1104033837@gmail.com>
2025-08-06 20:48:18 +0900

    [Add] 메일 전송 기능 확인용 엔드포인트 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly mailerService: MailerService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('test-mail')
+  async testMail() {
+    await this.mailerService.sendMail({
+      to: 'algo2000@naver.com',
+      subject: 'Test Email',
+      text: 'This is a test email',
+    });
   }
 }

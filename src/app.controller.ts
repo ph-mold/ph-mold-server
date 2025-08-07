0b26ee632f5bb2e4d86bfd9aa952660d01b03f00
l11040 <l1104033837@gmail.com>
2025-08-07 16:11:31 +0900

    [Chore] 메일 전송 기능 관련 코드 제거

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,25 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { MailerService } from '@nestjs-modules/mailer';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly mailerService: MailerService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Get('test-mail')
-  async testMail() {
-    await this.mailerService.sendMail({
-      to: 'algo2000@naver.com',
-      subject: 'Test Email',
-      text: 'This is a test email',
-    });
   }
 }
